2c94d776ca58de532c517ee952b54e2442db9d47
WebDeveloper <mvunam@gmail.com>
2026-02-14 16:01:10 +0200

    fix: add .gitignore, remove node_modules and dist from tracking - Fixes Vercel permission denied error on webpack binary

--- src/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const game_1 = require("./game");
-const canvas = document.getElementById('plinkoCanvas');
-const ctx = canvas.getContext('2d');
-canvas.width = 800;
-canvas.height = 600;
-const game = new game_1.Game(canvas, ctx);
-game.start();
